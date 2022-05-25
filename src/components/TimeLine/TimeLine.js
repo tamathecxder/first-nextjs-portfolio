b562821f9ede565b8c057429d855553e0896f209
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-05-25 16:03:18 +0700

    Putting svg in carouselContainer

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -19,7 +19,7 @@ const Timeline = () => {
 
     if (carouselRef.current) {
       const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length));
-      
+
       scroll(carouselRef.current, scrollLeft);
     }
   }
@@ -54,7 +54,7 @@ const Timeline = () => {
         <>
           {TimeLineData.map((item, index) => (
             <CarouselMobileScrollNode key={index} final={index === TOTAL_CAROUSEL_COUNT - 1}>
-              <CarouselItem 
+              <CarouselItem
                 index={index}
                 id={`carousel_item-${index}`}
                 active={activeItem}
@@ -62,6 +62,36 @@ const Timeline = () => {
               >
                 <CarouselItemTitle>
                   {item.year}
+                  <CarouselItemImg
+                    width="208"
+                    height="6"
+                    viewBox="0 0 208 6"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M2.5 5.5C3.88071 5.5 5 4.38071 5 3V3.5L208 3.50002V2.50002L5 2.5V3C5 1.61929 3.88071 0.5 2.5 0.5C1.11929 0.5 0 1.61929 0 3C0 4.38071 1.11929 5.5 2.5 5.5Z"
+                      fill="url(#paint0_linear)"
+                      fill-opacity="0.33"
+                    />
+                    <defs>
+                      <linearGradient
+                        id="paint0_linear"
+                        x1="-4.30412e-10"
+                        y1="0.5"
+                        x2="208"
+                        y2="0.500295"
+                        gradientUnits="userSpaceOnUse">
+                        <stop stop-color="white" />
+                        <stop
+                          offset="0.79478"
+                          stop-color="white"
+                          stop-opacity="0"
+                        />
+                      </linearGradient>
+                    </defs>
+                  </CarouselItemImg>
                 </CarouselItemTitle>
               </CarouselItem>
             </CarouselMobileScrollNode>
