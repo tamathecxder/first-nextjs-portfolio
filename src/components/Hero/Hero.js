b301d0671848a253c2b3fcf104efbbdbc763701e
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-05-25 12:35:48 +0700

    Creating a structure on the hero section and at the same time styling it

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello there!,<br />
+        I'm Yudistira Eka Pratama
+      </SectionTitle>
+      <SectionText>
+      Someone who is enthusiastic of technology and other programming stuff, especially in the web programming field. Since I learned about programming, I finally have a dream to become a professional web developer who masters full stack front-end and back-end on technology websites.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
